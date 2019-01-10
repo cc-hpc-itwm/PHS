6baa221d51a057e7ba22654c23e50bc4a3f56f31
Peter Habelitz <peter.michael.habelitz@itwm.fraunhofer.de>
2019-01-10 13:25:19 +0100

    changed functional_test_dask.py to a minimal example for client.upload_file error

--- phs/functional_test_dask.py
+++ phs/functional_test_dask.py
@@ -1,18 +1,23 @@
 import os
 from dask.distributed import Client, as_completed
 
-def task(hyperpar):
+repository_root_dir = '/home/HabelitzP' #(example:'/home/NAME')
+import sys
+sys.path.append(repository_root_dir + '/parallel_hyperparameter_search/')
+from examples import test_functions
+
+'''def task(hyperpar):
     exec(hyperpar, globals(), globals())
     result = x * y
-    return result
+    return result'''
 
 DASK_MASTER_IP = os.environ['DASK_MASTER_IP']
 DASK_MASTER_PORT = os.environ['DASK_MASTER_PORT']
 sub_future = []
 parameter_string_list = ['x=1\ny=2','x=2\ny=4','x=3\ny=6']
 with Client(DASK_MASTER_IP + ':' + DASK_MASTER_PORT, timeout='20s') as client:
-    #client.upload_file('/home/HabelitzP/phs/test_functions.py')
+    client.upload_file('/home/HabelitzP/parallel_hyperparameter_search/examples/test_functions.py')
     for s in parameter_string_list:
-        sub_future.append(client.submit(task, s))
+        sub_future.append(client.submit(test_functions.test_griewank, s))
     for f in as_completed(sub_future):
         print(f.result()) # errors occuring during execution of future f inside workers are fetched and reraised with the result() method
