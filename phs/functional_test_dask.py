658238c034daead058cbf70baa39eeac7c3397bc
Peter Habelitz <peter.michael.habelitz@itwm.fraunhofer.de>
2019-04-01 14:40:11 +0200

    debub CI

--- phs/functional_test_dask.py
+++ phs/functional_test_dask.py
@@ -24,10 +24,12 @@ sub_future = []
 parameter_string_list = ['x=1\ny=2', 'x=2\ny=4', 'x=3\ny=6']
 with Client(DASK_MASTER_IP + ':' + DASK_MASTER_PORT, timeout='20s') as client:
     pp.pprint(client.scheduler_info())
-    client.upload_file('/home/HabelitzP/parallel_hyperparameter_search/examples/test_functions.py')
+    client.upload_file(
+        '/home/HabelitzP/parallel_hyperparameter_search/examples/func_def/basic_test_functions.py')
     for s in parameter_string_list:
         parameter = {'hyperpar': s}
-        sub_future.append(client.submit(examples.func_def.test_functions.test_griewank, parameter))
+        sub_future.append(client.submit(
+            examples.func_def.basic_test_functions.test_griewank, parameter))
     for f in as_completed(sub_future):
         # errors occuring during execution of future f inside workers are fetched and reraised with the result() method
         print(f.result())
